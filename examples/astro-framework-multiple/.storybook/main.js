89b80bdd0f2da019ba977e4b3b92886a83f74bf8
Scott Nath <github@scottnath.com>
2023-05-16 17:47:29 -0400

    :crystal_ball: devcontainer for astro

--- examples/astro-framework-multiple/.storybook/main.js
+++ examples/astro-framework-multiple/.storybook/main.js
@@ -15,7 +15,7 @@ const config = {
   refs,
   async viteFinal(config, options) {
     // Ensures that the cache directory is inside the project directory
-    config.cacheDir = path.join(__dirname, '../node_modules/.vite')
+    config.cacheDir = path.join(__dirname, '../node_modules/.vite-composition')
     return config;
   },
 };
