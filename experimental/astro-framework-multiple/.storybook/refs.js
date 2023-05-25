6fdc1272ce2cddca6d5a7aae00c15cf1c42d903c
Scott Nath <github@scottnath.com>
2023-05-25 11:59:09 -0400

    Deploy multi-framework composition storybook to gh-pages (#3)

--- experimental/astro-framework-multiple/.storybook/refs.js
+++ experimental/astro-framework-multiple/.storybook/refs.js
@@ -1,18 +1,60 @@
-export default {
+/**
+ * @fileoverview StorybookRefs content generated according to destination environment
+ */
+
+/**
+ * StorybookRefs content for one Storybook instance
+ * @typedef {Object} StorybookReferences
+ * @property {string} title - Title of the Storybook instance
+ * @property {string} local - Local URL with port for the Storybook instance
+ * @property {string} ghpages - GitHub Pages path for the Storybook instance
+ */
+
+/**
+ * StorybookRefs content for all Storybook instances
+ * @typedef {Object.<string, StorybookReferences>} StorybookRefs
+ */
+export const references = {
   "astro_preact": {
     "title": "preact",
-    "url": "http://localhost:6001",
+    "local": "http://localhost:6001",
+    "ghpages": "/storydocker-examples/astro-framework-multiple/preact"
   },
   "astro_react": {
     "title": "react",
-    "url": "http://localhost:6002",
+    "local": "http://localhost:6002",
+    "ghpages": "/storydocker-examples/astro-framework-multiple/react"
   },
   "astro_svelte": {
     "title": "svelte",
-    "url": "http://localhost:6003",
+    "local": "http://localhost:6003",
+    "ghpages": "/storydocker-examples/astro-framework-multiple/svelte"
   },
   "astro_vue": {
     "title": "vue",
-    "url": "http://localhost:6004",
+    "local": "http://localhost:6004",
+    "ghpages": "/storydocker-examples/astro-framework-multiple/vue"
   },
+}
+
+
+
+/**
+ * Generates a Storybook ref (`{ title: string; url: string }`) for each child storybook. 
+ *  `url` is chosen based on destination environment
+ * @returns {Object.<string, import('@storybook/core-common').StorybookRefs>}
+ */
+export const refs = (config, { configType }) => {
+  let urlKey = 'ghpages';
+  if (configType === 'DEVELOPMENT') {
+    urlKey = 'local';
+  }
+  const newRefs = {};
+  for (const [key, value] of Object.entries(references)) {
+    newRefs[key] = {
+      title: value.title,
+      url: value[urlKey],
+    }
+  }
+  return newRefs;
 }
--- experimental/astro-framework-multiple/astro.config.mjs
+++ experimental/astro-framework-multiple/astro.config.mjs
@@ -5,8 +5,16 @@ import svelte from '@astrojs/svelte';
 import vue from '@astrojs/vue';
 import solid from '@astrojs/solid-js';
 
+let assetsPrefix = '/';
+if (process.env.DEPLOY_DEST === 'ghpages') {
+  assetsPrefix = '/storydocker-examples/astro-framework-multiple/';
+}
+
 // https://astro.build/config
 export default defineConfig({
 	// Enable many frameworks to support all different kinds of components.
 	integrations: [preact(), react(), svelte(), vue(), solid()],
+  build: {
+    assetsPrefix
+  }
 });
