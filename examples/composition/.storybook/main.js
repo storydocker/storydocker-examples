31aaeb87db2761fa25b9cd86a10bd83c793d8728
Scott Nath <github@scottnath.com>
2023-05-31 10:54:22 -0400

    :rocket: build vite ghpages versions (#4)

--- examples/composition/.storybook/main.js
+++ examples/composition/.storybook/main.js
@@ -1,9 +1,29 @@
 import path from "path";
-import refs from "./refs.js";
+import references from "./refs.js";
+
+/**
+ * Generates a set of Storybook refs (`{ title: string; url: string }`) for each child storybook. 
+ *  `url` is chosen based on destination environment
+ * @returns {Object.<string, import('@storybook/core-common').StorybookRefs>}
+ */
+export const refs = (config, { configType }) => {
+  let urlKey = 'ghpages';
+  if (configType === 'DEVELOPMENT') {
+    urlKey = 'local';
+  }
+  const newRefs = {};
+  for (const [key, value] of Object.entries(references)) {
+    newRefs[key] = {
+      title: value.title,
+      url: value[urlKey],
+    }
+  }
+  return newRefs;
+}
 
 /** @type { import('@storybook/html-vite').StorybookConfig } */
 const config = {
-  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../stories/**/*.mdx"],
   addons: ['@storybook/addon-essentials', '@storybook/addon-links', '@storybook/addon-interactions', '@storybook/addon-coverage'],
   framework: {
     name: "@storybook/html-vite",
