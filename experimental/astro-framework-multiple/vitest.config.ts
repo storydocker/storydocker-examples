673e19c326c9fa6f62581162e76f4c262869ebbc
Scott Nath <github@scottnath.com>
2023-06-26 19:06:48 -0400

    ⬆️ latest storybook; devops cleanup (#7)
    * updates storybook via storydocker-storybook module
    * gh-actions unit-tests show just astro coverage
    * all examples have dependency updates, but minor
    * bits and bobs to clean up test configs

--- experimental/astro-framework-multiple/vitest.config.ts
+++ experimental/astro-framework-multiple/vitest.config.ts
@@ -1,14 +1,25 @@
 /// <reference types="vitest" />
-import { getViteConfig } from 'astro/config';
+import { defineConfig } from 'vite'
+import {svelte} from '@sveltejs/vite-plugin-svelte'
+import preact from '@preact/preset-vite'
+import react from "@vitejs/plugin-react";
+import vue from '@vitejs/plugin-vue'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-import config from 'storydocker-vitest/vitest.config.js'
-
-export default getViteConfig({
-  ...config,
+export default defineConfig({
+  plugins: [tsconfigPaths(), svelte({hot: !process.env.VITEST}), vue(), react(), preact()],
 	test: {
-    ...config.test,
     alias: {
       '@/': new URL('./src/', import.meta.url).pathname,
     },
-  }
+    environment: 'jsdom',
+    setupFiles: ['./tests/setup.js'],
+    coverage: {
+      all: true,
+      include: ['src/**/*.jsx', 'src/**/*.tsx', 'src/**/*.vue', 'src/**/*.svelte'],
+      exclude: ['src/main.jsx', 'src/main.tsx', 'src/components/SolidCounter.tsx'],
+      provider: 'istanbul',
+      reporter: ['text', 'json', 'html'],
+    },
+	},
 });
