5e61ed6ddfa487f96c6d732890c2293bd87d1764
Scott Nath <github@scottnath.com>
2023-07-11 12:52:20 -0400

    :rocket: deploy astro to chromatic

--- experimental/astro-framework-multiple/.storybook/refs.js
+++ experimental/astro-framework-multiple/.storybook/refs.js
@@ -18,22 +18,26 @@ export const references = {
   "astro_preact": {
     "title": "preact",
     "local": "http://localhost:6001",
-    "ghpages": "/storydocker-examples/astro-framework-multiple/preact"
+    "ghpages": "/storydocker-examples/astro-framework-multiple/preact",
+    "chromatic": "https://64ad68bd58a29190829b21c9-ldyqiukppu.chromatic.com"
   },
   "astro_react": {
     "title": "react",
     "local": "http://localhost:6002",
-    "ghpages": "/storydocker-examples/astro-framework-multiple/react"
+    "ghpages": "/storydocker-examples/astro-framework-multiple/react",
+    "chromatic": "https://645cf8cc6c66c97beb256bf6-aotbylwlaj.chromatic.com"
   },
   "astro_svelte": {
     "title": "svelte",
     "local": "http://localhost:6003",
-    "ghpages": "/storydocker-examples/astro-framework-multiple/svelte"
+    "ghpages": "/storydocker-examples/astro-framework-multiple/svelte",
+    "chromatic": "https://64ad7443637336c6b8b81971-wygblehcro.chromatic.com"
   },
   "astro_vue": {
     "title": "vue",
     "local": "http://localhost:6004",
-    "ghpages": "/storydocker-examples/astro-framework-multiple/vue"
+    "ghpages": "/storydocker-examples/astro-framework-multiple/vue",
+    "chromatic": "https://64ad74a7637336c6b8b81b84-bkvrxzvlxp.chromatic.com/"
   },
 }
 
@@ -48,6 +52,9 @@ export const refs = (config, { configType }) => {
   let urlKey = 'ghpages';
   if (configType === 'DEVELOPMENT') {
     urlKey = 'local';
+  }
+  if (process.env.DEPLOY_DEST === 'chromatic') {
+    urlKey = 'chromatic';
   }
   const newRefs = {};
   for (const [key, value] of Object.entries(references)) {
