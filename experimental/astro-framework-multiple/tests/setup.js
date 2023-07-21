1e2318271610284bce00df8da42155690646a738
Scott Nath <github@scottnath.com>
2023-07-21 02:48:08 -0400

    :white_check_mark: fix test configs

--- experimental/astro-framework-multiple/tests/setup.js
+++ experimental/astro-framework-multiple/tests/setup.js
@@ -1,6 +1,8 @@
 import { expect, afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
-import { cleanup as svelteCleanup } from '@testing-library/svelte'
+import { cleanup as svelteCleanup } from '@testing-library/svelte';
+import { cleanup as preactCleanup } from '@testing-library/preact';
+import { cleanup as vueCleanup } from '@testing-library/vue'
 import matchers from '@testing-library/jest-dom/matchers';
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -11,4 +13,6 @@ expect.extend(matchers);
 afterEach(() => {
   cleanup();
   svelteCleanup();
+  preactCleanup();
+  vueCleanup();
 });
